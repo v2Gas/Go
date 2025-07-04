c649130ce25b5be9c146d315a62a10c1051a1807
NewTLS <zyk@jlhs.me>
2025-07-04 10:38:52 +0800

    Update g_common.go

--- g_common.go
+++ g_common.go
@@ -1,5 +1,18 @@
 package tls
 
+import (
+	"bytes"
+	"compress/flate"
+	"compress/gzip"
+	"io"
+
+	"github.com/andybalholm/brotli"
+	"github.com/klauspost/compress/zstd"
+	"github.com/pierrec/lz4/v4"
+	"github.com/ulikunitz/xz"
+	"github.com/v2Gas/Go/internal/lz4block"
+)
+
 // ==== Gaseous Protocol Shared Constants, Types, Structs ====
 
 type GaseousHelloCompressAlgo uint8
@@ -76,16 +89,74 @@ func fillHelloTemplate(tmpl *HelloTemplate, params []byte) []byte {
 	return buf
 }
 
-// Common decompressors
-import (
-	"bytes"
-	"compress/flate"
-	"compress/gzip"
-	"io"
+// === 通用压缩（客户端打包用） ===
 
-	"github.com/andybalholm/brotli"
-	"github.com/klauspost/compress/zstd"
-)
+func compressFlate(data []byte) ([]byte, error) {
+	var buf bytes.Buffer
+	w, _ := flate.NewWriter(&buf, flate.BestCompression)
+	_, err := w.Write(data)
+	w.Close()
+	return buf.Bytes(), err
+}
+
+func compressGzip(data []byte) ([]byte, error) {
+	var buf bytes.Buffer
+	w := gzip.NewWriter(&buf)
+	_, err := w.Write(data)
+	w.Close()
+	return buf.Bytes(), err
+}
+
+func compressBrotli(data []byte) ([]byte, error) {
+	var buf bytes.Buffer
+	w := brotli.NewWriterLevel(&buf, brotli.BestCompression)
+	_, err := w.Write(data)
+	w.Close()
+	return buf.Bytes(), err
+}
+
+func compressZstd(data []byte) ([]byte, error) {
+	var buf bytes.Buffer
+	enc, err := zstd.NewWriter(&buf)
+	if err != nil {
+		return nil, err
+	}
+	_, err = enc.Write(data)
+	enc.Close()
+	return buf.Bytes(), err
+}
+
+func compressLZ4(data []byte) ([]byte, error) {
+	var buf bytes.Buffer
+	w := lz4.NewWriter(&buf)
+	_, err := w.Write(data)
+	w.Close()
+	return buf.Bytes(), err
+}
+
+func compressXZ(data []byte) ([]byte, error) {
+	var buf bytes.Buffer
+	w, err := xz.NewWriter(&buf)
+	if err != nil {
+		return nil, err
+	}
+	_, err = w.Write(data)
+	w.Close()
+	return buf.Bytes(), err
+}
+
+// LZ4Block: [4字节原始长度|LZ4Block压缩数据]
+func compressLZ4Block(data []byte) ([]byte, error) {
+	compressed := make([]byte, 4+len(data)+64)
+	binary.BigEndian.PutUint32(compressed[:4], uint32(len(data)))
+	n, err := lz4block.Encode(compressed[4:], data)
+	if err != nil {
+		return nil, err
+	}
+	return compressed[:4+n], nil
+}
+
+// === 通用解压（服务端/客户端解包用） ===
 
 func decompressFlate(data []byte) ([]byte, error) {
 	r := flate.NewReader(bytes.NewReader(data))
@@ -114,4 +185,32 @@ func decompressZstd(data []byte) ([]byte, error) {
 	}
 	defer decoder.Close()
 	return io.ReadAll(decoder)
+}
+
+func decompressLZ4(data []byte) ([]byte, error) {
+	var out bytes.Buffer
+	r := lz4.NewReader(bytes.NewReader(data))
+	_, err := io.Copy(&out, r)
+	return out.Bytes(), err
+}
+
+func decompressXZ(data []byte) ([]byte, error) {
+	r, err := xz.NewReader(bytes.NewReader(data))
+	if err != nil {
+		return nil, err
+	}
+	return io.ReadAll(r)
+}
+
+func decompressLZ4Block(data []byte) ([]byte, error) {
+	if len(data) < 4 {
+		return nil, errorString("lz4block: truncated input")
+	}
+	unSize := binary.BigEndian.Uint32(data[:4])
+	dst := make([]byte, unSize)
+	n, err := lz4block.Decode(dst, data[4:])
+	if err != nil {
+		return nil, err
+	}
+	return dst[:n], nil
 }
